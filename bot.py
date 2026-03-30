f18c454ea08deb962bf4520330b02281c540766f
Sebastian Klinga <sebbe.klinga@hotmail.com>
2026-03-30 21:54:58 +0200

    v2

--- bot.py
+++ bot.py
@@ -4,12 +4,15 @@ import json
 import time
 import os
 
-URL = "https://marknad.studentbostader.se/widgets/?pagination=1&paginationantal=20&callback=jQuery37106019219408482263_1774893162413&widgets%5B%5D=objektfilter%40fordon&widgets%5B%5D=objektsortering&widgets%5B%5D=paginering%40fordon&widgets%5B%5D=objektlista%40fordon&widgets%5B%5D=pagineringgonew%40fordon&widgets%5B%5D=pagineringlista%40fordon&widgets%5B%5D=pagineringgoold%40fordon&_=1774893162414"
+DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+URL = "https://marknad.studentbostader.se/widgets/?pagination=1&paginationantal=20&callback=jQuery37106689919178412977_1774894629215&widgets%5B%5D=objektfilter%40fordon&widgets%5B%5D=objektsortering&widgets%5B%5D=paginering%40fordon&widgets%5B%5D=objektlista%40fordon&widgets%5B%5D=pagineringgonew%40fordon&widgets%5B%5D=pagineringlista%40fordon&widgets%5B%5D=pagineringgoold%40fordon&_=1774894629216"
 FILENAME = "history.txt"
 
 def skicka_notis(text):
-    url = "DISCORD_WEBHOOK_URL"
-    requests.post(url, json={"content": text})
+    if not DISCORD_WEBHOOK_URL:
+        print("Fel: Hittade ingen Discord Webhook URL i systemet!")
+        return
+    requests.post(DISCORD_WEBHOOK_URL, json={"content": text})
 
 def kolla_annonser():
     print(f"[{time.strftime('%H:%M:%S')}] Kollar Studentbostäder...")
@@ -18,13 +21,17 @@ def kolla_annonser():
     raw_text = response.text
     
     # Rensa jQuery-omslag
-    if "(" in raw_text:
-        clean_json = raw_text[raw_text.find("(")+1 : raw_text.rfind(")")]
-        data = json.loads(clean_json)
-    else:
-        data = response.json()
-
-    annonser = data["data"]["objektlista@fordon"]
+    try:
+        if "(" in raw_text:
+            clean_json = raw_text[raw_text.find("(")+1 : raw_text.rfind(")")]
+            data = json.loads(clean_json)
+        else:
+            data = response.json()
+        
+        annonser = data["data"]["objektlista@fordon"]
+    except Exception as e:
+        print(f"Kunde inte tolka datan: {e}")
+        return
     
     
     if os.path.exists(FILENAME):
@@ -42,9 +49,8 @@ def kolla_annonser():
             adress = annons["adress"]
             hyra = annons["hyra"]
             omrade = annons["omrade"]
-            plats_nr = annons["tinyObjektNr"] # Eller annons["platsNr"]
+            plats_nr = annons["tinyObjektNr"]
             inflytt = annons["inflyttningDatum"]
-            
             lank = "https://studentbostader.se" + annons["detaljUrl"]
 
             meddelande = (
@@ -56,23 +62,23 @@ def kolla_annonser():
                 f"📅 Tillträde: {inflytt}\n\n"
                 f"🔗 Direktlänk: {lank}"
             )
-            print(meddelande)
+            
+            print(f"Hittade ny: {adress}")
             skicka_notis(meddelande)
             sedda.append(annons_id)
             nya_hittade = True
 
+    # Spara historik
     if nya_hittade:
         with open(FILENAME, "w") as f:
             for s in sedda:
                 f.write(f"{s}\n")
+    else:
+        print("Inga nya annonser hittades just nu.")
     
-while True:
+
+if __name__ == "__main__":
     try:
-        print("Kollar efter nya bilplatser...")
         kolla_annonser()
-        
-                
     except Exception as e:
         print(f"Ett fel uppstod: {e}")
-    
-    time.sleep(20)
